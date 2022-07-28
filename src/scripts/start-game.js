dbbf9adad1234488847cd7e1f418ec4c0c573fc3
Andrew Dutson <Dutsandrew@gmail.com>
2022-07-27 18:04:11 -0600

    battleships height is calculated by client browser and added to board

--- src/scripts/start-game.js
+++ src/scripts/start-game.js
@@ -1,3 +1,5 @@
+import { doc } from "prettier";
+
 export {
   createGameBoards,
   startGameButton,
@@ -47,11 +49,82 @@ function counter() {
 }
 
 function startGameButton() {
-  console.log('starting game');
+  const computerContainer = document.querySelector('#computer-container');
+  if (computerContainer.classList.contains('computer-container-hidden')) {
+    return;
+  }
+  onlyDisplayPlayerOneBoard();
+  addBoatSelectionBox();
+  addBoatsToBox();
+}
+
+function onlyDisplayPlayerOneBoard() {
   const gameBoard = document.querySelector('#game-board');
   const computerContainer = document.querySelector('#computer-container');
   gameBoard.classList.remove('game-board-hidden');
   computerContainer.classList.add('computer-container-hidden');
+}
+
+function addBoatSelectionBox() {
+  const gameBoard = document.querySelector('#game-board');
+  const boatContainer = document.createElement('div');
+    boatContainer.setAttribute('id', 'boat-container');
+  const labelForPlacingBoat = document.createElement('p');
+    labelForPlacingBoat.setAttribute('id', 'label-for-placing-boat');
+    labelForPlacingBoat.textContent = 'Place Your Boats Captain!';
+  const boatSelectionBox = document.createElement('div');
+    boatSelectionBox.setAttribute('id', 'boat-selection-box');
+  boatContainer.appendChild(labelForPlacingBoat);
+  boatContainer.appendChild(boatSelectionBox);
+  gameBoard.appendChild(boatContainer);
+}
+
+function addBoatsToBox() {
+  const boatSelectionBox = document.querySelector('#boat-selection-box');
+  const cellWidth = document.querySelector('.game-board-cell').clientWidth;
+  const cellHeight = document.querySelector('.game-board-cell').clientHeight;
+
+  const carrierIcon = document.createElement('div');
+    carrierIcon.setAttribute('id', 'carrier-player');
+    carrierIcon.classList.add('player-boat');
+    const carrierHeight = cellHeight * 5 + 7;
+    carrierIcon.style.height = `${carrierHeight}px`;
+    carrierIcon.style.width = `${cellWidth}px`;
+    carrierIcon.draggable = 'true';
+  const battleshipIcon = document.createElement('div');
+    battleshipIcon.setAttribute('id', 'battleship-player');
+    battleshipIcon.classList.add('player-boat');
+    const battleshipHeight = cellHeight * 4 + 7;
+    battleshipIcon.style.height = `${battleshipHeight}px`;
+    battleshipIcon.style.width = `${cellWidth}px`;
+    battleshipIcon.draggable = 'true';
+  const cruiserIcon = document.createElement('div');
+    cruiserIcon.setAttribute('id', 'cruiser-player');
+    cruiserIcon.classList.add('player-boat');
+    const cruiserHeight = cellHeight * 3 + 6;
+    cruiserIcon.style.height = `${cruiserHeight}px`;
+    cruiserIcon.style.width = `${cellWidth}px`;
+    cruiserIcon.draggable = 'true';
+  const submarineIcon = document.createElement('div');
+    submarineIcon.setAttribute('id', 'submarine-player');
+    submarineIcon.classList.add('player-boat');
+    const submarineHeight = cellHeight * 3 + 6;
+    submarineIcon.style.height = `${submarineHeight}px`;
+    submarineIcon.style.width = `${cellWidth}px`;
+    submarineIcon.draggable = 'true';
+  const destroyerIcon = document.createElement('div');
+    destroyerIcon.setAttribute('id', 'destroyer-player');
+    destroyerIcon.classList.add('player-boat');
+    const destroyerHeight = cellHeight * 2 + 3;
+    destroyerIcon.style.height = `${destroyerHeight}px`;
+    destroyerIcon.style.width = `${cellWidth}px`;
+    destroyerIcon.draggable = 'true';
+
+  boatSelectionBox.appendChild(carrierIcon);
+  boatSelectionBox.appendChild(battleshipIcon);
+  boatSelectionBox.appendChild(cruiserIcon);
+  boatSelectionBox.appendChild(submarineIcon);
+  boatSelectionBox.appendChild(destroyerIcon);
 }
 
 function resetGameButton() {
