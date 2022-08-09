42cc16d4654a2840d91bb6d9c5b92c62eeb657d8
Andrew Dutson <Dutsandrew@gmail.com>
2022-08-09 10:32:29 -0600

    computer boat placement is randomly generated

--- src/scripts/game-loop.js
+++ src/scripts/game-loop.js
@@ -1,4 +1,4 @@
-import { Ship } from "./Ship";
-import { Gameboard } from "./Gameboard";
-import { Ship } from "./Ship";
+import { Ship } from './Ship';
+import { Gameboard } from './Ship';
+import { Player } from './Player';
 
--- src/scripts/start-game.js
+++ src/scripts/start-game.js
@@ -555,11 +555,221 @@ function buildComputerBoard() {
   const axisSwitch = document.querySelector('#axis-switch');
     axisSwitch.remove();
   
-  placeComputerBoats();
+  generateComputerBoatStartingPoint();
 }
 
-function placeComputerBoats() {
+function generateComputerBoatStartingPoint() {
+  let computerCarrierStart = Math.floor(Math.random() * 100);
+  let computerBattleshipStart = Math.floor(Math.random() * 100);
+  let computerCruiserStart = Math.floor(Math.random() * 100);
+  let computerSubmarineStart = Math.floor(Math.random() * 100);
+  let computerDestroyerStart = Math.floor(Math.random() * 100);
 
+  // validates that starting points are 5 away to prevent bad placement
+  while (
+    Math.abs(computerCarrierStart - computerBattleshipStart) < 5
+    || Math.abs(computerCarrierStart - computerCruiserStart) < 5
+    || Math.abs(computerCarrierStart - computerSubmarineStart) < 5
+    || Math.abs(computerCarrierStart - computerDestroyerStart) < 5
+  
+    || Math.abs(computerBattleshipStart - computerCarrierStart) < 5
+    || Math.abs(computerBattleshipStart - computerCruiserStart) < 5
+    || Math.abs(computerBattleshipStart - computerSubmarineStart) < 5
+    || Math.abs(computerBattleshipStart - computerDestroyerStart) < 5
+
+    || Math.abs(computerCruiserStart - computerCarrierStart) < 5
+    || Math.abs(computerCruiserStart - computerBattleshipStart) < 5
+    || Math.abs(computerCruiserStart - computerSubmarineStart) < 5
+    || Math.abs(computerCruiserStart - computerDestroyerStart) < 5
+
+    || Math.abs(computerSubmarineStart - computerCarrierStart) < 5
+    || Math.abs(computerSubmarineStart - computerBattleshipStart) < 5
+    || Math.abs(computerSubmarineStart - computerCruiserStart) < 5
+    || Math.abs(computerSubmarineStart - computerDestroyerStart) < 5
+
+    || Math.abs(computerDestroyerStart - computerCarrierStart) < 5
+    || Math.abs(computerDestroyerStart - computerBattleshipStart) < 5
+    || Math.abs(computerDestroyerStart - computerCruiserStart) < 5
+    || Math.abs(computerDestroyerStart - computerSubmarineStart) < 5
+    ) {
+      computerCarrierStart = Math.floor(Math.random() * 100);
+      computerBattleshipStart = Math.floor(Math.random() * 100);
+      computerCruiserStart = Math.floor(Math.random() * 100);
+      computerSubmarineStart = Math.floor(Math.random() * 100);
+      computerDestroyerStart = Math.floor(Math.random() * 100);
+  }
+
+  buildComputerBoats(computerCarrierStart, computerBattleshipStart, computerCruiserStart, computerSubmarineStart, computerDestroyerStart);
+}
+
+function buildComputerBoats(computerCarrierStart, computerBattleshipStart, computerCruiserStart, computerSubmarineStart, computerDestroyerStart) {
+  const carrierCoordinates = calculateBoatPlacement('carrier', computerCarrierStart);
+  const battleshipCoordinates = calculateBoatPlacement('battleship', computerBattleshipStart);
+  const cruiserCoordinates = calculateBoatPlacement('cruiser', computerCruiserStart);
+  const submarineCoordinates = calculateBoatPlacement('submarine', computerSubmarineStart);
+  const destroyerCoordinates = calculateBoatPlacement('destroyer', computerDestroyerStart);
+
+  // validates that calculated cells are not duplicates
+  const fixedBoats = verifyNoDuplicateCoordinates(carrierCoordinates, battleshipCoordinates, cruiserCoordinates, submarineCoordinates, destroyerCoordinates);
+
+  if (fixedBoats === 'no bad match') {
+    placeComputerBoats(carrierCoordinates, battleshipCoordinates, cruiserCoordinates, submarineCoordinates, destroyerCoordinates);
+  } else if (fixedBoats !== 'no bad match') {
+    placeComputerBoats(fixedBoats[0], fixedBoats[1], fixedBoats[2], fixedBoats[3], fixedBoats[4]);
+  }
+}
+
+function calculateBoatPlacement(boatType, boatStart) {
+
+  // random number to pick whether to place boat on x/y-axis
+  const randomNumber = Math.floor(Math.random() * 10);
+
+  let firstAdditionalCell;
+  let secondAdditionalCell;
+  let thirdAdditionalCell;
+  let fourthAdditionalCell;
+
+  // place on y-axis
+  if (randomNumber % 2 === 0) {
+    if (boatStart > 50) {
+      firstAdditionalCell = boatStart - 10;
+      secondAdditionalCell = boatStart - 20;
+      thirdAdditionalCell = boatStart - 30;
+      fourthAdditionalCell = boatStart - 40;
+    }
+
+    if (boatStart < 50) {
+      firstAdditionalCell = boatStart + 10;
+      secondAdditionalCell = boatStart + 20;
+      thirdAdditionalCell = boatStart + 30;
+      fourthAdditionalCell = boatStart + 40;
+    }
+  }
+
+  // place on x-axis
+  if (randomNumber % 2 !== 0) {
+    if (getLastDigit(boatStart) <= 5) {
+      firstAdditionalCell = boatStart + 1;
+      secondAdditionalCell = boatStart + 2;
+      thirdAdditionalCell = boatStart + 3;
+      fourthAdditionalCell = boatStart + 4;
+    }
+  
+    if (getLastDigit(boatStart) > 5) {
+      firstAdditionalCell = boatStart - 1;
+      secondAdditionalCell = boatStart - 2;
+      thirdAdditionalCell = boatStart - 3;
+      fourthAdditionalCell = boatStart - 4;
+    }
+
+    if (boatStart % 10 === 0) {
+      firstAdditionalCell = boatStart - 1;
+      secondAdditionalCell = boatStart - 2;
+      thirdAdditionalCell = boatStart - 3;
+      fourthAdditionalCell = boatStart - 4;
+    }
+  }
+
+  if (boatType === 'carrier') {
+    return [
+      boatStart,
+      firstAdditionalCell,
+      secondAdditionalCell,
+      thirdAdditionalCell,
+      fourthAdditionalCell
+    ]
+  } else if (boatType === 'battleship') {
+    return [
+      boatStart,
+      firstAdditionalCell,
+      secondAdditionalCell,
+      thirdAdditionalCell
+    ]
+  } else if (boatType === 'cruiser' || boatType === 'submarine') {
+    return [
+      boatStart,
+      firstAdditionalCell,
+      secondAdditionalCell
+    ]
+  } else if (boatType === 'destroyer') {
+    return [
+      boatStart,
+      firstAdditionalCell
+    ]
+  }
+}
+
+function verifyNoDuplicateCoordinates(
+  carrierCoordinates,
+  battleshipCoordinates,
+  cruiserCoordinates,
+  submarineCoordinates,
+  destroyerCoordinates
+  ) {
+    let fixedCarrier;
+    let fixedBattleship;
+    let fixedCruiser;
+    let fixedSubmarine;
+    let fixedDestroyer;
+    let badMatch = false;
+
+    while (checkIfArraysShareValue(carrierCoordinates, battleshipCoordinates) === 'duplicate coordinate found' 
+    || checkIfArraysShareValue(carrierCoordinates, cruiserCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(carrierCoordinates, submarineCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(carrierCoordinates, destroyerCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(battleshipCoordinates, carrierCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(battleshipCoordinates, cruiserCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(battleshipCoordinates, submarineCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(battleshipCoordinates, destroyerCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(cruiserCoordinates, carrierCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(cruiserCoordinates, battleshipCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(cruiserCoordinates, submarineCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(cruiserCoordinates, destroyerCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(submarineCoordinates, carrierCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(submarineCoordinates, battleshipCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(submarineCoordinates, cruiserCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(submarineCoordinates, destroyerCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(destroyerCoordinates, carrierCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(destroyerCoordinates, battleshipCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(destroyerCoordinates, cruiserCoordinates) === 'duplicate coordinate found'
+    || checkIfArraysShareValue(destroyerCoordinates, submarineCoordinates) === 'duplicate coordinate found') {
+      badMatch = true;
+      fixedCarrier = calculateBoatPlacement('carrier', carrierCoordinates[0]);
+      fixedBattleship = calculateBoatPlacement('battleship', battleshipCoordinates[0]);
+      fixedCruiser = calculateBoatPlacement('cruiser', cruiserCoordinates[0]);
+      fixedSubmarine = calculateBoatPlacement('submarine', submarineCoordinates[0]);
+      fixedDestroyer = calculateBoatPlacement('destroyer', destroyerCoordinates[0]);
+    }
+
+    if (badMatch === false) {
+      return 'no bad match';
+    } else if (badMatch === true) {
+      return [
+        fixedCarrier,
+        fixedBattleship,
+        fixedCruiser,
+        fixedSubmarine,
+        fixedDestroyer
+      ]
+    }
+}
+
+function checkIfArraysShareValue(array1, array2) {
+  const filteredArray = array1.filter(value => array2.includes(value));
+  if (filteredArray.length > 0) {
+    return 'duplicate coordinate found';
+  } else if (filteredArray.length === 0) {
+    return 'no duplicate coordinates found';
+  }
+}
+
+function placeComputerBoats(carrierCoordinates, battleshipCoordinates, cruiserCoordinates, submarineCoordinates, destroyerCoordinates) {
+  console.log(carrierCoordinates, battleshipCoordinates, cruiserCoordinates, submarineCoordinates, destroyerCoordinates);
+}
+
+function getLastDigit(number) {
+  const convertedNumber = Number(number.toString().slice(-1));
+  return convertedNumber;
 }
 
 function resetGameButton() {
